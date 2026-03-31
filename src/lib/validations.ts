a69a54a042704f7ae60e289822f125cef0b5c3e2
tenmenezes <tenn.menezes@gmail.com>
2026-03-31 10:46:12 -0300

    Feature: Adicionando validações de registro do usuário

---
+++ src/lib/validations.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+/*
+    ? Inserção de validações do registro do usuário
+*/
+
+export const registerSchema = z.object({
+    name: z
+        .string()
+        .min(2, "Nome deve conter pelo menos 2 caracteres")
+        .max(255, "Nome muito longo"),
+
+    email: z
+        .string()
+        .email("Email inválido")
+        .max(255, "Email muito longo"),
+
+    password: z
+        .string()
+        .min(8, "A senha deve ter pelo menos 8 caractares"),
+
+});
