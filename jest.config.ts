e1dc5c5eafe73b3779449c08601ce88253e42bef
tenmenezes <tenn.menezes@gmail.com>
2026-04-01 10:03:35 -0300

    Feature: Adicionando configurações iniciais para iniciar os testes em Jest.

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import nextJest from "next/jest.js";
+import type { Config } from "jest";
+
+const createJestConfig = nextJest({
+    dir: "./",
+});
+
+const config: Config = {
+    testEnvironment: "node",
+    clearMocks: true,
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/src/$1",
+    },
+    testMatch: ["**/__tests__/**/*.test.ts"],
+};
+
+export default createJestConfig(config);
